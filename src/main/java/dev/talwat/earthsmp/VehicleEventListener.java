ecdd0d00ad669e3249d78cfad86c4fffc5701394
talwat <83217276+talwat@users.noreply.github.com>
2024-06-29 21:44:09 +0200

    fix: fix vehicle permissions

--- src/main/java/dev/talwat/earthsmp/VehicleEventListener.java
+++ src/main/java/dev/talwat/earthsmp/VehicleEventListener.java
@@ -6,27 +6,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.*;
 
 public class VehicleEventListener implements Listener {
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onVehicleCreate(VehicleCreateEvent event) {
         event.setCancelled(false);
     }
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onVehicleBreak(VehicleDestroyEvent event) {
         event.setCancelled(false);
     }
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onVehicleEnter(VehicleEnterEvent event) {
         event.setCancelled(false);
     }
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onVehicleExit(VehicleExitEvent event) {
         event.setCancelled(false);
     }
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onVehicleDamage(VehicleDamageEvent event) {
         event.setCancelled(false);
     }
