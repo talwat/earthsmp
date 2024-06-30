64b5d5d079ce96d0e9b8ebe4109f91f2ba3c4d17
talwat <83217276+talwat@users.noreply.github.com>
2024-06-30 21:40:32 +0200

    fix: allow villager damage

--- src/main/java/dev/talwat/earthsmp/EventListener.java
+++ src/main/java/dev/talwat/earthsmp/EventListener.java
@@ -16,7 +16,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -56,12 +55,12 @@ public class EventListener implements Listener {
         event.setCancelled(true);
     }
 
-    @EventHandler(priority = EventPriority.LOW)
-    public void onDamage(EntityDamageEvent event) {
-        if (event.getEntity().getType() == EntityType.VILLAGER) {
-            event.setCancelled(true);
-        }
-    }
+//    @EventHandler(priority = EventPriority.LOW)
+//    public void onDamage(EntityDamageEvent event) {
+//        if (event.getEntity().getType() == EntityType.VILLAGER) {
+//            event.setCancelled(true);
+//        }
+//    }
 
     @EventHandler(priority = EventPriority.LOW)
     public void onDamageByEntity(EntityDamageByEntityEvent event) {
