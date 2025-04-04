1674726f113b0df1aff4311617b8d4dc7e58dd55
talwat <83217276+talwat@users.noreply.github.com>
2025-04-04 11:30:10 +0200

    feat: start work on roles

--- ui/server/middleware/auth.ts
+++ ui/server/middleware/auth.ts
@@ -1,16 +1,28 @@
 import { getServerSession } from "#auth";
+import { readFile } from "fs/promises";
 
 export default eventHandler(async (event) => {
   const session = await getServerSession(event);
 
-  if (
-    !session &&
-    !event.path.startsWith("/api/auth") &&
-    event.path.startsWith("/api")
-  ) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: "Server Unauthorized",
-    });
+  if (event.path.startsWith("/api/auth") && !event.path.startsWith("/api")) {
+    return;
   }
+
+  let roles = readFile("./roles.json");
+
+  let name = session?.user?.name;
+  if (session && name != null && name in roles) {
+    let role = roles[name as keyof typeof roles];
+
+    if (role == "admin") {
+      return;
+    } else if (role == "journalist" && event.path.startsWith("/api/news")) {
+      return;
+    }
+  }
+
+  throw createError({
+    statusCode: 401,
+    statusMessage: "Unauthorized",
+  });
 });
