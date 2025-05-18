76da0ea5488ceeaca55a0dc8e6a46aef4c121935
talwat <83217276+talwat@users.noreply.github.com>
2025-05-18 14:53:45 +0200

    chore: switch over to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("idea")
+    java
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly(libs.io.papermc.paper.paper.api)
+    compileOnly(libs.xyz.jpenilla.squaremap.api)
+}
+
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks.runServer {
+    minecraftVersion("1.19.4")
+}
+
+group = "dev.talwat"
+version = "1.5.0-SNAPSHOT"
